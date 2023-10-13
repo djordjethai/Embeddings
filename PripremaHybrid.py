d3374779df2eaae5cdfe219be8ef8f404fabf6c1
Djordje <djordje.thai@gmail.com>
2023-10-13 16:29:45 +0700

    prefix space

--- PripremaHybrid.py
+++ PripremaHybrid.py
@@ -245,7 +245,7 @@ def prepare_embeddings(chunk_size, chunk_overlap):
                     st.error("Prefiks nije na raspolaganju za ovaj chunk. {e}")
 
                 # # Specify the file name where you want to save the data
-                content = text_prefix + document.page_content
+                content = text_prefix + " " + document.page_content
                 txt_string += content.replace("\n", "") + "\n"
 
             napisano = st.info(
